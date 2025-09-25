734e14ee0cf46de5590f8e88f99d0991ff817b3c
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-09-25 21:05:13 +0300

    Create part-v-0002-logic1.go

---
+++ part-v-0002-logic1.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(false && false)
+
+	fmt.Println(true || true)
+	fmt.Println(false || true)
+	fmt.Println(false || false)
+}
+
+// true
+// false
+// false
+// true
+// true
+// false
