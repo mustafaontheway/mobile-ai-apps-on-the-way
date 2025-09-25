51ade3236d18ad2cffbf1112c71b705c31b3edcf
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-09-25 21:16:32 +0300

    Create part-v-0003-basic-types2.go

---
+++ part-v-0003-basic-types2.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	active := false
+
+	completed := true 
+
+	var isReady bool = true 
+
+	fmt.Println(active, completed, isReady) // false true true
+}
+
