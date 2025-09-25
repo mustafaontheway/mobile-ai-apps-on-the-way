7890e778ea21e8e5ef622598b61b8bd4c4160b51
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-09-25 21:14:00 +0300

    Create part-v-0003-basic-types1.go

---
+++ part-v-0003-basic-types1.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var year uint16 = 2025
+
+	var age = 17
+
+	var difference int = -100_000
+
+	var weeklySales uint32 = 4_500_000
+
+	fmt.Println(year, age, difference, weeklySales) // 2025 17 -100000 4500000
+}
+
