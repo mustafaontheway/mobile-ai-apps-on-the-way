454dc9ef910687b4d5578ef387c5e0fd1f594747
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-07 09:31:47 +0300

    Create part-y-0002-array1.ts

---
+++ part-y-0002-array1.ts
@@ -0,0 +1,5 @@
+let years: number[] = [2000, 2006, 2008, 2013, 2016, 2020, 2025]
+
+let names: string[] = ["Ayhan", "Hakan", "Bilge"]
+
+let mixData: (boolean | number | string)[] = [true, 1, "abc"]
