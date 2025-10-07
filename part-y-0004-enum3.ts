c877567327fca5913991f5cb139d8251a5e7452e
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-07 11:08:54 +0300

    Create part-y-0004-enum3.ts

---
+++ part-y-0004-enum3.ts
@@ -0,0 +1,10 @@
+enum Strategies {
+    Buy = "Buy",
+    Sell = "Sell",
+    Keep = "Keep",
+    Wait = "Wait"
+}
+
+let currentStrategy: Strategies = Strategies.Keep 
+
+console.log(currentStrategy) // Keep
