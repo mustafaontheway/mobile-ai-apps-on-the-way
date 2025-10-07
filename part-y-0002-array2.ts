cfdd1b00fea1d3fe282166d0d7bd186a7ff360ad
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-07 10:08:56 +0300

    Create part-y-0002-array2.ts

---
+++ part-y-0002-array2.ts
@@ -0,0 +1,8 @@
+let quarterlySalesByMonths: number[][] = [
+    [500, 400, 300],
+    [600, 800, 400],
+    [650, 870, 660],
+    [880, 950, 900]
+]
+
+console.log("July sales result: " + quarterlySalesByMonths[1][2]) // July sales result: 400
