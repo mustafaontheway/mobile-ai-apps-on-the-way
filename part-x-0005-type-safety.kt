db78556b9d874690bb22f6742ffd35217b48cc6b
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-13 11:44:01 +0300

    Create part-x-0005-type-safety.kt

---
+++ part-x-0005-type-safety.kt
@@ -0,0 +1,9 @@
+fun main() {
+    
+	var num = 22
+    
+    //num = 3.21 // Assignment type mismatch: actual type is 'Double', but 'Int' was expected.
+    
+    //num = "hi" // Assignment type mismatch: actual type is 'String', but 'Int' was expected
+}
+
