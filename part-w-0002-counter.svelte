691885aeb2a359434cfc9b846c5dedd3de648466
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-13 22:45:54 +0300

    Create part-w-0002-counter.svelte

---
+++ part-w-0002-counter.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+
+  let counter = 0
+
+  function inc() {
+    counter++
+  }
+
+  function decr() {
+    counter--
+  }
+
+</script>
+
+<main>
+
+  <button onclick={inc}>Counter ++</button>
+
+  <p>Counter value: {counter}</p>
+
+  <button onclick={decr}>Counter --</button>
+  
+</main>
+
