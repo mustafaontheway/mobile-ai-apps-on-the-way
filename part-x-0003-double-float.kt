9a56e50b925307e71b6186874ba045cde413ab0b
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-13 11:35:30 +0300

    Create part-x-0003-double-float.kt

---
+++ part-x-0003-double-float.kt
@@ -0,0 +1,14 @@
+fun main() {
+    
+    val pi1 = 22.0 / 7 // double
+    
+    println(pi1)
+    
+    val pi2 = 22f / 7 // float
+    
+    println(pi2)
+   
+}
+
+// 3.142857142857143
+// 3.142857
