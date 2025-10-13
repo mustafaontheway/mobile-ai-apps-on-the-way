88d759c4daf477c9477624f3afe7bb1da8ae022b
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-13 11:40:24 +0300

    Create part-x-0004-string-interpolation.kt

---
+++ part-x-0004-string-interpolation.kt
@@ -0,0 +1,10 @@
+fun main() {
+    
+    val pi = 22.0 / 7 
+    
+    val radius = 5
+    
+    println("Radius: $radius and Area: ${pi * radius * radius}") // Radius: 5 and Area: 78.57142857142857
+    
+}
+
