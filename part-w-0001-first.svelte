88a4deabd7a227be375c6bbfca482df16e8a19a2
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-13 22:40:21 +0300

    Create part-w-0001-first.svelte

---
+++ part-w-0001-first.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+  let myName = "Mustafa Büyükdereli"
+  let year = 2025
+  let whySvelte = "Svelte for XXX! (I know what XXX means)"
+
+</script>
+
+<main>
+
+  <p>{myName}</p>
+  <p>Year: {year}</p>
+  <p>{whySvelte}</p>
+  
+</main>
+
