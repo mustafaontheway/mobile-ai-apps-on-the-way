6d6055d653398b77528a13de1ab3ebd15c3ce005
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-13 12:57:39 +0300

    Create part-x-0006-type-conversion1.kt

---
+++ part-x-0006-type-conversion1.kt
@@ -0,0 +1,20 @@
+fun main() {
+    
+	var num = 22 // int
+    
+    var rate: Double = 3.21 // double
+    
+    var pi = 3.14f
+    
+    println(num.toDouble())
+    
+    println(num.toFloat())
+    
+    println(rate.toInt())
+    
+    println(pi.toLong())
+    
+    println(pi.toInt())
+    
+}
+
