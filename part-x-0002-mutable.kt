a8965b60e3f102020858a0f810fdc339e93bf6a7
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-13 11:29:00 +0300

    Create part-x-0002-mutable.kt

---
+++ part-x-0002-mutable.kt
@@ -0,0 +1,16 @@
+fun main() {
+    
+   // var is mutable, not constant
+    
+    var salesTotal = 22_500_000
+    
+    println(salesTotal)
+    
+    salesTotal += 500_000 
+    
+    println(salesTotal)
+   
+}
+
+// 22500000
+// 23000000
