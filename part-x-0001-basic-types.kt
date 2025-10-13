5dce6e44102ef124a09f2de9208c4271a14cd2f3
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-13 11:24:40 +0300

    Create part-x-0001-basic-types.kt

---
+++ part-x-0001-basic-types.kt
@@ -0,0 +1,18 @@
+fun main() {
+    
+	// kotlin is NOT rust! if not required, i'll prefer: var age = 12
+    // but when you look at my rust code, you see: let age: u8 = 12;
+    
+    var budgetYear = 2025
+    
+    var salesTotal = 22_500_000
+    
+    var companyName = "Company Akis"
+    
+    var expectedProfitRate = 0.12
+    
+    var completed = true
+    
+    var aLetter = 'a' 
+   
+}
