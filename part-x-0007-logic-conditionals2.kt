3e075d6502a1117b0bdafae4a693b385768e0c0f
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-14 23:09:38 +0300

    Create part-x-0007-logic-conditionals2.kt

---
+++ part-x-0007-logic-conditionals2.kt
@@ -0,0 +1,20 @@
+fun main() {
+    
+	val x = 22 
+    
+    val y = 11 
+    
+ 	if (x > y) {
+        
+        println("1.")
+        
+    } else if (y > x) {
+        
+        println("2.")
+        
+    } else {
+        
+        println("3.")
+    }
+    
+}
