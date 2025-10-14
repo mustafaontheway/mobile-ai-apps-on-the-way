1ce943c2d74e597474873fb4a7057e9649a5caa9
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-14 23:15:01 +0300

    Create part-x-0008-when1.kt

---
+++ part-x-0008-when1.kt
@@ -0,0 +1,17 @@
+fun main() {
+    
+	val month = 5
+    
+    when (month) {
+        
+        6 -> println("June... Summer!")
+        7 -> println("July... Summer!")
+        8 -> println("August... Summer!")
+        
+        else -> {
+            
+            println("Not a summer month!")
+        }
+    }
+    
+}
