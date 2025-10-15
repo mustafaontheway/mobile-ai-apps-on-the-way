9c2baa5ddd70a8606e6c2f12fb45282432c99ed2
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-15 11:40:13 +0300

    Create part-x-0009-for-loop1.kt

---
+++ part-x-0009-for-loop1.kt
@@ -0,0 +1,15 @@
+fun main() {
+    
+    for (year in 2010..2015) {
+        
+        println("Year: $year")
+    }
+    
+}
+
+// Year: 2010
+// Year: 2011
+// Year: 2012
+// Year: 2013
+// Year: 2014
+// Year: 2015
