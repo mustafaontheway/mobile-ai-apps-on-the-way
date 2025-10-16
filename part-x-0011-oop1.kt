61ee859c2361b5a88ed1501ea6e85dbe3900f163
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-16 11:55:25 +0300

    Implement Employee class with properties and main function

---
+++ part-x-0011-oop1.kt
@@ -0,0 +1,23 @@
+class Employee(var name: String, var department: String, var salary: Int) {
+    
+}
+
+
+fun main() {
+    
+    val emp_ayhan = Employee("Ayhan Bilir", "HR", 62_000)
+    
+    println(emp_ayhan.name)
+    println(emp_ayhan.department)
+    println(emp_ayhan.salary)
+    
+    emp_ayhan.salary = 65_000
+    
+    println(emp_ayhan.salary)
+   
+}
+
+// Ayhan Bilir
+// HR
+// 62000
+// 65000
