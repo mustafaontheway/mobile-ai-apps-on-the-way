3cf1ca4490a3ae03e4deb86e8e2874c13c4e32b0
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-28 00:18:22 +0300

    Create part-x-0014-null-safety3.kt

---
+++ part-x-0014-null-safety3.kt
@@ -0,0 +1,9 @@
+fun main() {
+    
+	lateinit var data: String
+    
+    data = "Under the assumption of pure competition..."
+    
+    //lateinit val mySentence: String // 'lateinit' modifier is allowed only on mutable properties.
+
+}
