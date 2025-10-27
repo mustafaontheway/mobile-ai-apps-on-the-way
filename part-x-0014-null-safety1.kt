982b96cfed4c39bcd7947bfa74b11b552cdb7f59
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-28 00:12:07 +0300

    Create part-x-0014-null-safety1.kt

---
+++ part-x-0014-null-safety1.kt
@@ -0,0 +1,25 @@
+fun main() {
+    
+    var name: String? = null
+    
+    println(name)
+    
+    println(name?.uppercase())
+    
+    // assert the variable is not null
+    
+    //println(name!!.uppercase())
+    
+    name = "Ayhan"
+    
+    println(name)
+    
+    //var year: Int = null // Null cannot be a value of a non-null type 'Int'
+
+    var year: Int = 0
+   
+}
+
+// null
+// null
+// Ayhan
