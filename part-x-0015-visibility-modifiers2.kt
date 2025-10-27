335ac29e0a7b1cde890f9b281668f4d0d5731cb2
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-28 00:37:11 +0300

    Create part-x-0015-visibility-modifiers2.kt

---
+++ part-x-0015-visibility-modifiers2.kt
@@ -0,0 +1,42 @@
+class A {
+    
+    val a = "Hi"
+    
+    public val b = 21
+    
+    internal val c = 3.14 
+    
+    private val d = "Lorem"
+    
+    protected val e = 2025
+    
+}
+
+
+class B {
+    
+    val incomingData = A()
+    
+    init {
+        
+        println(incomingData.a)
+        println(incomingData.b)
+        println(incomingData.c)
+        
+        //println(incomingData.d) //Cannot access 'val d: String': it is private in 'A'.
+        
+        //println(incomingData.e) // Cannot access 'val e: Int': it is protected in 'A'.
+        
+    }
+    
+}
+
+fun main() {
+    
+    val bInstance = B()
+    
+}
+
+// Hi
+// 21
+// 3.14
