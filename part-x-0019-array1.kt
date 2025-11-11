efb7ad1952624eff6b53bf87842039cc28bfd2f9
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-11-11 22:13:58 +0300

    Create part-x-0019-array1.kt

---
+++ part-x-0019-array1.kt
@@ -0,0 +1,15 @@
+fun main() {
+    
+	val years = arrayOf(2010, 2008, 2121, 2014, 2045) // array length is constant! no insert, add, append...
+    
+    println(years[years.size - 1])
+    
+    println(years.get(4))
+    
+    println(years[4])
+   
+}
+
+// 2045
+// 2045
+// 2045
